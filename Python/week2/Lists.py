fb8ad390f4e14fcd4512980e252e1b99f51c136d
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-08-11 13:15:55 +0300

    lists

--- Python/week2/Lists.py
+++ Python/week2/Lists.py
@@ -24,14 +24,41 @@ list1.append([10,11])
 print(list1) 
 # [1, 2, 3, 4, 5, 3, 6, 8, 9, [10, 11]]
 
+even_numbers = [2, 4, 6]
+print(even_numbers)
 
+odd_numbers = [1, 3, 5]
+print(odd_numbers)
+ 
+even_numbers.extend(odd_numbers)
+print(even_numbers)
 # pop- deleting an item
 list1.pop(3)
 print(list1) 
-
+# delete second element
 del list1[1]
 print(list1) 
+
+# delete first 2 elements
+del list1[0:2]
+
+# del last
+del list1[-1]
 
 # listing all items in a list
 for item in list1:
     print(item)
+
+    # removing element
+    fruits =["orange","melon","apple","mango"]
+    fruits.remove("mango")
+    print(fruits)
+    print(fruits.count("apple"))
+
+    fruits.sort()
+    print(fruits)
+
+    # python list comprehension
+    numbers = [number*number for number in range(1,6)]
+    print(numbers)
+    # [1, 4, 9, 16, 25]
