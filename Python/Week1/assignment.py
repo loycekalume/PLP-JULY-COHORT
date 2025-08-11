4b7c1ed36141dc47c102808bb2fc463c36a34bd6
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-08-11 08:08:49 +0300

    python assignment

---
+++ Python/Week1/assignment.py
@@ -0,0 +1,31 @@
+def addition():
+    num1 = int(input("Enter firstNumber:"))
+    num2= int(input("Enter secondNumber:"))
+    sum= num1 + num2
+    print("The sum is: ", sum)
+
+
+def subtraction():
+    num1 = int(input("Enter firstNumber:"))
+    num2= int(input("Enter secondNumber:"))
+    subtraction= num1 - num2
+    print("The subtraction is: ", subtraction)
+
+
+def multiplication():
+    num1 = int(input("Enter firstNumber:"))
+    num2= int(input("Enter secondNumber:"))
+    multiplication = num1 * num2
+    print("The multiplication is: ", multiplication)
+
+
+def division():
+    num1 = int(input("Enter firstNumber:"))
+    num2= int(input("Enter secondNumber:"))
+    division = num1 / num2
+    print("The division is: ", division)
+
+addition()
+subtraction()
+division()
+multiplication()
